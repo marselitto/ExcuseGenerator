f53e341e0970c7480ed8024cb5c25b65aeff466d
szybek <jakub.jaworowski@cyfroweszkoly.pl>
2025-04-11 09:30:14 +0200

    Second Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,66 @@
 import './App.css'
+import {useState} from "react";
+
+interface FormData {
+  name: string;
+  powod: string;
+  wiarygodnosc: string;
+  data: string;
+  kreatywnosc: string;
+  komentarz: string;
+  pilna: boolean;
+}
+
 
 function App() {
 
+  const Form = () => {
+    const [formData, setFormData] = useState<FormData>({
+      data: "",
+      komentarz: "",
+      kreatywnosc: "",
+      pilna: false,
+      wiarygodnosc: "",
+      name: "", powod: "" });
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+  }
+
+
+
   return (
     <>
-      <form onSubmit={}>
+      <form onSubmit={handleSubmit}>
         <p>Podaj imie</p>
-        <input type={"text"}
-        />
+        <input type={"text"} name={"name"}/>
         <br/>
-        <p>Podaj powod wymowki</p>
-        <select>
+        <p>Podaj donwod wymowki</p>
+        <select name={"powod"}>
+          <option selected disabled>Wybierz powód</option>
           <option value={"spoznienie"}>Spóźnienie</option>
           <option value={"pracadomowa"}>Niedostarczenie pracy domowej</option>
           <option value={"brakodp"}>Brak odpowiedzi na wiadomość</option>
         </select>
         <br/>
         <p>Poziom wiarygodnosci</p>
-        <input type={"range"} />
+        <input type={"range"} name={"wiarygodnosc"} min={0} max={6}/>
         <br/>
         <p>Data wydarzenia</p>
-        <input type={"date"}
-        />
+        <input type={"date"} name={"data"}/>
         <br/>
         <p>Poziom kreatywnosci</p>
-        <select>
-            <option value="nw">nw</option>
+        <select value={"kreatywnosc"}>
+            <option selected disabled>Wybierz poziom</option>
         </select>
         <br/>
         <p>Dodatkowe szczegoly</p>
-        <textarea placeholder={"Dodaj swoj komentarz"}>
-
-        </textarea>
+        <textarea placeholder={"Dodaj swoj komentarz"} name={"komentarz"}/>
         <br/>
         <label>
           Pilna wymówka:
-          <input type={"checkbox"}
-          />
-
+          <input type={"checkbox"} name={"pilna"}/>
         </label>
         <br/>
         <button type='submit'>Submit</button>
